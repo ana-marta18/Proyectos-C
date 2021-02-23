52cc196661dc5a7c4f28ebdc634a1fe4d29e5a00
ana-marta18 <anamartasandoval01@gmail.com>
2021-02-22 23:06:01 -0600

    Hola Mundo

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hola_mundo_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------");
+            Console.WriteLine("¡Hola mundo!");
+            Console.WriteLine("------------");
+        }
+    }
+}
